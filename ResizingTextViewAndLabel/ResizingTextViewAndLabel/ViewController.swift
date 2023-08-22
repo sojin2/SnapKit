5a3c79684dc716f2fac4549a21e25add07db4290
ssooo2164 <ssooo2164@gmail.com>
2023-08-22 11:47:16 +0900

    Snapkit

---
+++ ResizingTextViewAndLabel/ResizingTextViewAndLabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResizingTextViewAndLabel
+//
+//  Created by 김소진 on 2023/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
